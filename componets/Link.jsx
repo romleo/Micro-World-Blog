63d457007bc4aab2483a45f96657cb0b4b390932
Roman <romandybinetc@gmail.com>
2020-03-11 11:19:05 +0200

    Create component4

---
+++ componets/Link.jsx
@@ -0,0 +1,6 @@
+import React, { forwardRef } from 'react';
+import { useRouter } from 'next/router';
+import clsx from 'clsx';
+import NextLink from 'next/link';
+import MuiLink from '@material-ui/core/Link';
+import PropTypes from 'prop-types';
