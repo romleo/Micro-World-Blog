37c32d244fb58de7005cfb2f857f42574fd40a76
Roman <romandybinetc@gmail.com>
2020-03-11 12:29:54 +0200

    Create component5

--- componets/Link.jsx
+++ componets/Link.jsx
@@ -4,3 +4,49 @@ import clsx from 'clsx';
 import NextLink from 'next/link';
 import MuiLink from '@material-ui/core/Link';
 import PropTypes from 'prop-types';
+
+const NextComposed = forwardRef( function NextComposed(props ,ref ){
+  const { href,as, prefetc,...other}= props;
+  return(
+    <NextLink href ={href} as={as} prefetch={prefetch}>
+      <a ref={ref} {...other}/>  
+    </NextLink>  
+  );   
+});
+
+NextComposed.propTypes={
+ as: PropTypes.string,
+ prefetch: PropTypes.func,
+ props: PropTypes.object,
+ href: PropTypes.string,  
+}
+
+const Link = ({
+  href,
+  className :classNameProps,
+  activeClassName = 'active',
+  ... other
+}) => {
+  const router = useRouter();
+  const pathname =typeof href === 'string'? href:href.pathname;
+  const classname =clsx(classNameProps,{
+    [activeClassName]:ruter.pathname===pathname && activeClassName  
+  }) 
+
+  return (
+   <MuiLink
+     component = {NextComposed}
+     className = {className}
+     href ={ href} 
+     {...other}
+    />
+   );
+ };
+
+ Link.propTypes={
+  activeClassName : PropsType.string,
+  className:PropTypes.string,
+  href:PropTypes.string,   
+ }
+
+ export default Link;
